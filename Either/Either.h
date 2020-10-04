a6e0eae3a3b7dda115ab9eb1cb721f7a83f17390
t-osawa-009 <da87435@gmail.com>
2020-10-04 15:07:30 +0900

    Initial Commit

---
+++ Either/Either.h
@@ -0,0 +1,18 @@
+//
+//  Either.h
+//  Either
+//
+//  Created by Takuya Ohsawa on 2020/10/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Either.
+FOUNDATION_EXPORT double EitherVersionNumber;
+
+//! Project version string for Either.
+FOUNDATION_EXPORT const unsigned char EitherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Either/PublicHeader.h>
+
+
